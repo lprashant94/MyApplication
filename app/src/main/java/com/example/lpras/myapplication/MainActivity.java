8ff29c70b50d86e9766c48aafe2d029d7cacff51
Prashant Lokhande <lprashant94@gmail.com>
2016-04-19 13:20:41 +0530

    Nickname

--- app/src/main/java/com/example/lpras/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lpras/myapplication/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.lpras.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_continue = (Button) findViewById(R.id.button_continue);
+        button_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edittext_nickname= (EditText) findViewById(R.id.editText);
+                button_continue.setText("NickName is " + edittext_nickname.getText());
+            }
+        });
+        
     }
 }
